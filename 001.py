3239df9700cbfe4550146f91d85ac8fa97480564
Fellipe Castro <contact@fellipecastro.com>
2014-12-27 14:29:26 -0200

    Update repository

--- 001.py
+++ 001.py
@@ -11,9 +11,16 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 from __future__ import print_function
 
 
-def euler_001(limit):
-    return sum(i for i in range(limit) if i % 3 == 0 or i % 5 == 0)
+def euler_001_a(limit):
+    s1, s2 = set(range(0, limit, 3)), set(range(0, limit, 5))
 
-response = euler_001(1000)
+    return sum(s1.union(s2))
+
+
+def euler_001_b(limit):
+    return sum(i for i in xrange(limit) if not (i % 3 and i % 5))
+
+response = euler_001_a(1000)
+# response = euler_001_b(1000)
 
 print(response)
